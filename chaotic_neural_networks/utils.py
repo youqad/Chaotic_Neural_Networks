8757779cd6eca5be0a8a96e21a8f2260a805b02a
youkad <youqad@gmail.com>
2018-06-13 06:06:13 +0200

    line break fix

--- chaotic_neural_networks/utils.py
+++ chaotic_neural_networks/utils.py
@@ -5,16 +5,13 @@ from scipy import signal, sparse
 # Target functions
 
 def periodic(t, amp=3., freq=1/600):
+    """Generates a periodic function which a sum of 4 sinusoids.
     """
-    Generates a periodic function which a sum of 4 sinusoids.
-    """
-    return amp*np.sin(np.pi*freq*t) + (amp/2) * np.sin(2*np.pi*freq*t) +\
-    (amp/3) * np.sin(3*np.pi*freq*t) + (amp/4) * np.sin(4*np.pi*freq*t)
+    return amp*np.sin(np.pi*freq*t) + (amp/2) * np.sin(2*np.pi*freq*t) + (amp/3) * np.sin(3*np.pi*freq*t) + (amp/4) * np.sin(4*np.pi*freq*t)
 periodic = np.vectorize(periodic)
 
 def triangle(t, freq=1/600, amp=3):
-    """
-    Generates a triangle-wave function
+    """Generates a triangle-wave function.
     """
     return amp*signal.sawtooth(2*np.pi*freq*t, 0.5)
 triangle = np.vectorize(triangle)
