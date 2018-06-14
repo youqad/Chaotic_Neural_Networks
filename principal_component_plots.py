2f6e563492195abfb39c59d60a8c4c906afb2673
youkad <youqad@gmail.com>
2018-06-14 09:41:37 +0200

    up

--- principal_component_plots.py
+++ principal_component_plots.py
@@ -4,8 +4,17 @@ import numpy as np
 
 t_max = 2400
 
+# Target function: Triangle-wave
+#network = networkA.NetworkA()
+
 # Target function: Sum of sinusoids
-network = networkA.NetworkA()
+#network = networkA.NetworkA(f=utils.periodic)
+
+# Target functions: sum of sinusoids AND triangle-wave
+#network = networkA.NetworkA(nb_outputs=2, f=utils.per_tri)
+
+# Target functions: sum of sinusoids AND triangle-wave AND cosine
+network = networkA.NetworkA(nb_outputs=3, f=utils.per_tri_cos)
 
 for _ in np.arange(0, t_max, network.dt):
     network.step(train_test='train')
